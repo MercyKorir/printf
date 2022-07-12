0b283ad529e25c983f235ec967c5252dbcfa15d4
MercyKorir <mcthegreat78@gmail.com>
2022-07-12 11:09:07 -0700

    I have added the file printf and main

--- main.h
+++ main.h
@@ -1,8 +1,21 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/**
+* stuct spec - define structure
+* @op: operator
+* @func: function
+*/
+struct spec
+{
+	char *op;
+	int (*func)(va_list);
+};
+typedef struct spec spec_t;
+
 int _printf(const char *format, ...);
 int _putchar(char c);
+int own_pars(const char *format, spec_t func_list[], va_list ap);
 int print_string(char *str);
 
 #endif /*MAIN_H*/
--- printf.c
+++ printf.c
@@ -1,58 +1,26 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdarg.h>
-#include <stdbool.h>
 
 /**
- * _printf - produces output acc toformat
- * @format: pointer
- *
- * Return: total number of characters excluding \0
- */
-
+* _printf - produces output according to format
+* @format: pointer argument
+* Return: number of characters
+*/
 int _printf(const char *format, ...)
 {
-	int n;
-	int total = 0;
-	bool flag = false;
+	int _charpr;
+	spec_t func_list[] = {
+		{"c", _char},
+		{"s", _str},
+		{"%", _perc},
+		{"d", _int},
+		{"i", _int}
+	};
 	va_list ap;
 
 	if (format == NULL)
 		return (0);
 	va_start(ap, format);
-	for (n = 0 ; *(format + n) != '\0' ; n++)
-	{
-		if (format[n] == '%')
-			flag = true;
-		else if (flag == true)
-		{
-			flag = false;
-			switch (format[n])
-			{
-				case 'c':
-					_putchar(va_arg(ap, int));
-					total += 1;
-					break;
-				case 's':
-					total += print_string(va_arg(ap, char *));
-					break;
-				case '%':
-					_putchar('%');
-					total += 1;
-					break;
-				default:
-					_putchar('%');
-					_putchar(format[n]);
-					total += 2;
-			};
-		}
-		else
-		{
-			_putchar(format[n]);
-			total += 1;
-		}
-	}
+	_charpr = own_pars(format, func_list, ap);
 	va_end(ap);
-
-	return (total);
+	return (_charpr);
 }
