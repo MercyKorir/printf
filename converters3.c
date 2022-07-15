9cf0087849c07d9f55aa2ff65fbaf4a151f71654
vivian347 <vkinyua001@gmail.com>
2022-07-15 15:01:28 -0700

    r

--- converters3.c
+++ converters3.c
@@ -4,6 +4,49 @@ unsigned int _S(va_list ap, buffer_t *output, unsigned char flag,
 		int width, int precision, unsigned char len);
 unsigned int _p(va_list ap, buffer_t *output, unsigned char flag,
 		int width, int precision, unsigned char len);
+unsigned int _r(va_list ap, buffer_t *output, unsigned char flag,
+		int width, int precision, unsigned char len);
+unsigned int _R(va_list ap, buffer_t *output, unsigned char flag,
+		int width, int precision, unsigned char len);
+
+/**
+ * _r - rev str
+ * @ap: arg
+ * @output: struct
+ * @flag: flag
+ * @width: width
+ * @len: length
+ * @precision: prec
+ *
+ * Return: noof bytes stored
+ */
+
+unsigned int _r(va_list ap, buffer_t *output, unsigned char flag,
+		int width, int precision, unsigned char len)
+{
+	char *str, *null = "(null)";
+	int size, i, end;
+	unsigned int ret = 0;
+
+	(void)flag;
+	(void)len;
+	str = va_arg(ap, char *);
+	if (str == NULL)
+		return (_memcpy(output, null, 6));
+	for (size = 0; *(str + size); )
+		size++;
+	ret += string_width(output, flag, width, precision, size);
+	end = size - 1;
+	precision = (precision == -1) ? size : precision;
+	for (i = 0; end >= 0 && i < precision; i++)
+	{
+		ret += _memcpy(output, (str + end), 1);
+		end--;
+	}
+	ret += neg_width(output, ret, flag, width);
+	return (ret);
+}
+
 
 /**
  * _p - conv arg to hex and stores to buffer
